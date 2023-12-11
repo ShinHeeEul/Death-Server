0d5f83e5512f0c08d7a0b32cb9cfc88bf2ebc330
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-12-11 22:05:04 +0900

    fix : CalculatorTest.java AssertEqual()의 expected와 actual을 반대로 입력한 부분 수정

--- Calculator/test/CalculatorTest.java
+++ Calculator/test/CalculatorTest.java
@@ -17,25 +17,26 @@ class CalculatorTest {
     @Test
     @DisplayName("빈 문자열이 들어온 경우 0이 반환되어야 한다.")
     void blank_is_null() {
-        assertEquals(calculator.add(" "), 0);
+        assertEquals(0, calculator.add(" "));
+        assertEquals(0, calculator.add(null));
     }
 
     @Test
     @DisplayName("\"1,2\"인 들어온 경우 3을 반환되어야 한다.")
     void with_comma() {
-        assertEquals(calculator.add("1,2"),3);
+        assertEquals(3, calculator.add("1,2"));
     }
 
     @Test
     @DisplayName("\"1,2:3\"인 경우 6이 반환되어야 한다.")
     void with_comma_column() {
-        assertEquals(calculator.add("1,2:3"),6);
+        assertEquals(6, calculator.add("1,2:3"));
     }
 
     @Test
     @DisplayName("\"//d\\n1d2,3\"인 경우 6이 반환되어야 한다.")
     void with_custom() {
-        assertEquals(calculator.add("//d\n1d2,3"),6);
+        assertEquals(6, calculator.add("//d\n1d2,3"));
     }
 
     @Test
