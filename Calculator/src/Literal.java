bc602036292513fa8fdda53fbce39755fef206e9
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-12-15 01:20:20 +0900

    feat: Literal 직관성을 위해 리터럴 상수 클래스 추가

---
+++ Calculator/src/Literal.java
@@ -0,0 +1,7 @@
+public class Literal {
+    public static final String COLUMN = ":";
+    public static final String COMMA = ",";
+    public static final String BLANK = " ";
+    public static final String OR = "|";
+    public static final String CUSTOM_REGEX = "//(.)\n(.*)";
+}
